1a4e2a8701ccf970ec83570d9e74087df7d02cac
mting314 <57047134+mting314@users.noreply.github.com>
2021-05-12 00:36:05 -0700

    build

--- assets/js/search-worker-9.0.3.js
+++ assets/js/search-worker-9.0.3.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2021-05-11T03:32:11-07:00';
+const INDEX_KEY = 'index--2021-05-12T00:35:00-07:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-importScripts("/assets/js/service-worker.js?t=2021-05-11T03:32:11-07:00");
+importScripts("/assets/js/service-worker.js?t=2021-05-12T00:35:00-07:00");
